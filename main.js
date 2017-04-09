01ec4dcdd6a02dd671409ae29f658f03b4233c43
Regor24 <regor124@gmail.com>
2017-04-09 20:16:00 +0700

    kek

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Анонимная самоисполняющаяся функция и событие, когда загружен ХТМЛ и построено ДОМ дерево
 (() => {
   // // Если поместить объявление через переменную после вызова, то её не будет видно
-  const ready = () => {
+  var ready = () => {
     console.log('ready');
   }
   document.addEventListener('DOMContentLoaded', ready);
@@ -9,31 +9,34 @@
 })();
 
 // Правильный хук на событие transition
-(() => {
-  window.onload = () => {
-    var keys = document.querySelectorAll('.transition-hook__block');
+var keys = document.querySelectorAll('.transition-hook__block');
 
-    function removeEffect(e) {
-      // Суть - в данный момент событие transitionend срабатывает много раз на разные события - это зависит от цсс - у нас там и бордер, и скейл и прочее.
-      // Нам нужна только трансформация-скейл, поэтому отлавливаем окончание этого события и тут же убираем класс.
-      if (e.propertyName !== 'transform') return;
-      this.classList.remove('active');
-    }
+function removeEffect(e) {
+  // Суть - в данный момент событие transitionend срабатывает много раз на разные события - это зависит от цсс - у нас там и бордер, и скейл и прочее.
+  // Нам нужна только трансформация-скейл, поэтому отлавливаем окончание этого события и тут же убираем класс.
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('active');
+}
 
-    keys.forEach(key => key.addEventListener('transitionend', removeEffect));
-    window.addEventListener("keydown", (e) => {
-      var key = document.querySelector('.transition-hook__block[data-key="' + e.keyCode + '"]');
-      if (!key) return;
-      key.classList.add('active');
-    });
-  }
-})();
+keys.forEach(key => key.addEventListener('transitionend', removeEffect));
+window.addEventListener("keydown", (e) => {
+  var key = document.querySelector('.transition-hook__block[data-key="' + e.keyCode + '"]');
+  if (!key) return;
+  key.classList.add('active');
+});
 
+// Let, const, var
+let fVar = function () {
+  console.log('sadsad!!');
+}
 
+// Spread & Rest
+function spread() {
+  var arr1 = ['1','2','3'];
+  var arr2 = [...arr1, 'z'];    // '1','2','3', 'z'
+  var arr3 = ['z', ...arr1] ;   // 'z','1','2','3'
+  console.log('spread', arr2);
+}
 
-
-
-var arr1 = ['1','2','3'];
-var arr2 = [...arr1, 'z'];    // '1','2','3', 'z'
-var arr3 = ['z', ...arr1] ;   // 'z','1','2','3'
-console.log(arr2);
+// LOAD TEST:
+spread();
